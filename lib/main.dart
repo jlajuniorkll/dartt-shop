d0f79eeb360507a3238c61b026d934e6670a9775
MacBook Junior <jlajuniorkll@icloud.com.br>
2022-03-09 02:05:41 -0300

    commit firestore ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dartt_shop/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
+  FirebaseFirestore.instance.collection('teste').add({'teste': 'teste'});
 }
 
 class MyApp extends StatelessWidget {
